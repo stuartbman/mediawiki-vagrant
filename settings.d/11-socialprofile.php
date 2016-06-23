78a9ee6d2dfcaee0a2af7b797c7826788be23672
Stu Maitland <stuart@mediwikis.com>
2016-06-23 23:01:48 +0100

    added extension extension SOcialProfile

---
+++ settings.d/11-socialprofile.php
@@ -0,0 +1,9 @@
+<?php
+// MediaWiki settings for Social Profile
+// Information: hhttps://www.mediawiki.org/wiki/Extension:RecentPages
+
+require_once("$IP/extensions/SocialProfile/SocialProfile.php");
+	$wgUserProfileDisplay['foes'] = false;
+	$wgUserProfileDisplay['stats'] = true;
+	$wgUserBoard = false;
+	$wgUserProfileDisplay['board'] = false;
