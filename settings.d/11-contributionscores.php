f34c2229b3f8ff61c7207e71ae88d304ca013724
Stu Maitland <stuart@mediwikis.com>
2016-06-23 21:17:30 +0100

    added more mediawiki files and ContributionScores & DataValues extensions

---
+++ settings.d/11-contributionscores.php
@@ -0,0 +1,5 @@
+<?php
+// MediaWiki settings for Contribution Scores
+// Information: https://www.mediawiki.org/wiki/Extension:Contribution_Scores
+
+include_once "$IP/extensions/ContributionScores/ContributionScores.php";
