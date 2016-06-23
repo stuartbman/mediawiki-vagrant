f2b600050ab7401c9811ac871bb42aa1d692aa49
Stu Maitland <stuart@mediwikis.com>
2016-06-23 22:11:18 +0100

    added extensionNoTitle

---
+++ settings.d/11-notitle.php
@@ -0,0 +1,6 @@
+<?php
+// MediaWiki settings for Loops
+// Information: https://www.mediawiki.org/wiki/Extension:Contribution_Scores
+
+require_once "$IP/extensions/NoTitle/NoTitle.php";
+$wgRestrictDisplayTitle = false;
